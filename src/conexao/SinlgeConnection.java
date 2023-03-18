23fe24aafde63effd71b0e8c720e8dd0f81a4660
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-03-18 10:02:41 -0300

    Classe de conexao com banco de dados

--- src/conexao/SinlgeConnection.java
+++ src/conexao/SinlgeConnection.java
@@ -1,6 +1,8 @@
 package conexao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+
 
 public class SinlgeConnection {
 	
@@ -8,5 +10,37 @@ public class SinlgeConnection {
 	private static String user = "postgres";
 	private static String password = "admin";
 	private static Connection connection = null;
+	
+	static {
+		
+		conectar();
+	}
+	
+	public SinlgeConnection() {
+		
+		conectar();
+	}
+	
+	private static void conectar() {
+		
+		try {
+			
+			if(connection == null) {
+				
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		
+		return connection;
+	}
 
 }
